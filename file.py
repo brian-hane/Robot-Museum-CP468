814bf4986def1d3ad03267d2e21e8551a87295b4
Sumeet Jhand <sumeetjhand@gmail.com>
2018-12-04 18:52:50 -0500

    Fixed file I/O: readFile function now takes a file as input,renamed input_test.txt to input.txt and corrected input, files now in root of git

---
+++ file.py
@@ -0,0 +1,92 @@
+"""  
+-------------------------------------------------------  
+file.py
+-------------------------------------------------------  
+Author:  Brian Hane, Sumeet Jhand
+_updated_ ="2018-12-04"
+-------------------------------------------------------  
+"""
+
+def readFile(fileName):
+    startList = []
+    enviro = []
+    try:
+        inputStream = open(fileName, "r")
+    except:
+        raise Exception("Could not locate input file {0}.".format(fileName))
+
+    # room layout and other info
+    curr = inputStream.readline().split()
+    roomHeight = int(curr[0])
+    roomWidth = int(curr[1])
+
+    # robot count
+    numBots = int(inputStream.readline())
+
+    # coordinates of robots
+    for _ in range(0, numBots):
+        botCoords = inputStream.readline().split()
+        location = []
+        for coord in botCoords:
+            location.append(int(coord))
+        startList.append(location)
+
+    # rendezvous location
+    rendev = []
+    rendevList = inputStream.readline().split()
+    rendev.append(int(rendevList[0]))
+    rendev.append(int(rendevList[1]))
+
+    # read layout of room points
+    for _ in range(0, roomHeight):
+        curr = inputStream.readline()
+        row = []
+        for c in curr:
+            if c == '1' or c == '0':
+                row.append(int(c))
+        enviro.append(row)
+
+    inputStream.close()
+    return enviro, startList, rendev
+
+
+'''def writeFile(result):
+    invalidFile = True
+   # filename = "output_test.txt"
+  #  outputStream = open("output_test.txt", "w", encoding="utf-8")
+
+    while invalidFile:
+        try:
+            filename = input("Enter output file name:")
+            if filename == "output_test.txt":
+                print("true")
+            else:
+                print("fuck")
+            outputStream = open("filename", "w", encoding="utf-8")
+
+            for x in result:
+                outputStream.write(str(x))
+                outputStream.write("\n")
+                print("file")
+            invalidFile = False
+        except:
+            print("File not found")
+
+    outputStream.close()
+    return'''
+
+if __name__ == '__main__':
+    # test
+    enviro, startList, rendev = readFile("input.txt")
+
+    print(enviro)
+    print(startList)
+    print(rendev)
+
+    '''arr = [100000001,
+        100000010,
+        100000010,
+        100000010,
+        100000010,
+        100000010]
+    writeFile(arr)'''

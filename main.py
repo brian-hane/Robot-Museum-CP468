c6d1ac61baca1eac2fc0e79c7fadc3fb92f3f3a0
Brian <hane1750@mylaurier.ca>
2018-12-06 11:50:02 -0500

    updated to include call for GUI function

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
+
+#from tkinter import *
+
 from aStar import aStar
+#from drawResult import *
+
 from file import readFile, writeFile
 
-inputFileName = raw_input("Enter the input file name (press Enter to default to \"input.txt\"): ") or "input.txt"
-outputFileName = raw_input("Enter the output file name (press Enter to default to \"result.txt\"): ") or "result.txt"
+
+inputFileName = raw_input(
+    "Enter the input file name (press Enter to default to \"input.txt\"): ") or "input.txt"
+outputFileName = raw_input(
+    "Enter the output file name (press Enter to default to \"result.txt\"): ") or "result.txt"
 
 enviro, startList, rendev = readFile(inputFileName)
 print("Loaded data from {0}.".format(inputFileName))
 
-solutions = aStar(enviro, startList, rendev)
+solutions, visited = aStar(enviro, startList, rendev)
 print("Successfully computed solutions.")
 
 writeFile("result.txt", rendev, solutions)
 print("Wrote solutions to {0}.".format(outputFileName))
 
 print("Exiting...")
+print(visited)
+pic = Tk()
+pic = frame_grid(visited, len(enviro), len(enviro[0]))
